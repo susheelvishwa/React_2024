36da153c5559b6f4796ad0183b17e8b5b4b2b59c
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-14 18:28:08 +0530

    click the button and show the card UI

---
+++ 23_01_2024/example2/mountface/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Task1 from './components/task1'
+function App() {
+
+  return (
+    <>
+      <h1>Testing</h1>
+      <Task1/>
+    </>
+  )
+}
+
+export default App
---
+++ 23_01_2024/example2/mountface/src/components/task1.jsx
@@ -0,0 +1,54 @@
+
+
+// Task1 is make a api request to response back data from the api and when user click the button the show thins data on UI
+
+import { useState } from "react";
+// make a api request from react Component
+// save this response from the server in localStorage
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  async function clickme() {
+    try {
+      console.log("clicked");
+      // make àapi request and get the data
+      let response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+
+      let finaldata = await response.json();
+      setData(finaldata);
+      // disply on this data on UI
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(data);
+
+  return (
+    <>
+      <h1>Saving api response in react component</h1>
+      <button onClick={clickme}>click me</button>
+      {data.map((todo) => (
+        <div
+          style={{
+            border: "1px solid black",
+            margin: "15px",
+            padding: "15px",
+            backgroundColor: "skyblue",
+            color: "black",
+          }}
+          key={todo.id}
+        >
+          <p>ID : {todo.id} </p>
+          <p>Product : {todo.title}</p>
+          <p>Completed : {todo.completed ? "yes" : "No"}</p>
+          <p>Email: {todo.email}</p>
+          <p>NAme:{todo.username}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default App;
