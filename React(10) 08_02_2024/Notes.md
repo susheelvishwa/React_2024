210f8b4dce9bbaa41a5d6c218479d05f924ad1f3
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-04-30 22:17:41 +0530

    update folder name

---
+++ React(10) 08_02_2024/Notes.md
@@ -0,0 +1,4 @@
+# react routing
+
+# SAP single page application
+react routing dom
---
+++ React(9) 06_02_2024/day1/demo2/src/component/Child.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Child = () => {
+  return (
+    <div>Child  : {}
+      {/* import child1 here */}
+    </div>
+  )
+}
+
+export default Child
