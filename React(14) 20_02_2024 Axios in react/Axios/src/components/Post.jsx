0e789732b5e918713b947596f83766bff9f3d634
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-06-24 23:15:01 +0530

    Axios

--- React(14) 20_02_2024 Axios in react/Axios/src/components/Post.jsx
+++ React(14) 20_02_2024 Axios in react/Axios/src/components/Post.jsx
@@ -3,9 +3,9 @@ import "../App.css";
 const Post = ({ name, department, company }) => {
   return (
     <div className="card">
-      <h2 className="name">name: {name} </h2>
-      <h2 className="department">department: {department} </h2>
-      <h2 className="company">company: {company} </h2>
+      <h2 className="name">Name: {name} </h2>
+      <h2 className="department">Department: {department} </h2>
+      <h2 className="company">Company: {company} </h2>
     </div>
   );
 };
